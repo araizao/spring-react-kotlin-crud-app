de18aedaec7d59d1d07cb3bedac79942e8768ba5
Oscar Araiza <oaraiza1@gmail.com>
2020-05-14 23:42:03 -0700

    wip

---
+++ kotlin-crud-app/client/src/Home.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import './App.css';
+import {Link} from 'react-router-dom';
+import {Button, Container} from 'reactstrap';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="app">
+                {this.props.navbar}
+                <Container fluid>
+                    <div>
+                        <Button color="secondary">
+                            <Link classname="app-link" to="/coffee-shops"> Manage Coffee Shops</Link>
+                        </Button>
+                    </div>
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default Home;
